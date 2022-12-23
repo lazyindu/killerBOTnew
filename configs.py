e109384c687057fae4fe9f521262ee2c1c2c3428
indu <70087467+lazyindu@users.noreply.github.com>
2022-12-23 13:54:03 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,16 +7,16 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.getenv("API_ID", 13323016))
-    API_HASH = os.getenv("API_HASH", "68e791e616100248b0a53ae86a661a12")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "5966226090:AAH-D0jYwYhq_8WE9mD0GQxjWI37PaPoE3Y")
+    API_ID = int(os.getenv("API_ID", 25882792))
+    API_HASH = os.getenv("API_HASH", "846699536cd018e1b55b7ea895156f08")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "5905448114:AAGZaRVTWU8aX-YZr5tRkc51ZcQ9hB-AIqc")
     BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "kill")
-    USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "BQBHq5ahdr_WgShwenFMGL4vwVxwX10NIrbtJ8n0_UvAQ5xmuF1DiVhZK7NWRkCokozkqCwXMgpvuCJVoeHjKId5QD4kiD5nLRlrFtaf4KF1QHFkSiZ57cRX3oSCeQZEotET2rHp0UKGab4aOrUYkBrntedU3vun8cofGM7stEXmqF54Egq4uCWRyK94qipsAiSCvwG2dy5mi4vjIofgECStfWDhK_IKYFQ7vwfBvvkSDBbqyZKdT3tTodvPhqelaFy1d_uLbjYOIuifvi0Gd8V8nC5jvdcShn3QUBO3MbwXThCEZ7wbxTxRmginp8z09GNxt0SXy8QMrpXXlNq53iTlAAAAAU8-YVYA")
-    CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1001835512708))
-    BOT_USERNAME = os.getenv("BOT_USERNAME", "LazyBroadCastzzBot")
-    BOT_OWNER = int(os.getenv("BOT_OWNER", 1484670284))
+    USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "BQCM8EbNjgeQKcvWFW5_Lt4efjkb-6_5d7CE0Nks1mrKFyIyARqQhm5n1mSYyqcxcfrifsMKGUr3iISOtFxYeXv4sGAYdVoBBldsDEWRNUWYyuk8Y721mq1YQJkSC9m6-Kv6H15XnJmeTlFXZhLrOr3G4UbSrSg9bh3RF44v8LJd1qGNj6iaN3E7Uh8qfCfjSbWCk6MCtRkFvimJS2A9JigTyxN3YmBL4j1T1_0bUNKaKUNBSHVbYXtWYxvfiJ1WPFaBjBdH17R-b98ykpumGfUaD05TR1xfRwX_wkthasTOQrXagMaln6TySKhRLM8heGvue4IeEXI4UFkY40oWTYcNW3jt7AA")
+    CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1001818441571))
+    BOT_USERNAME = os.getenv("BOT_USERNAME", "Primeflix_Cinema_Autofilter_bot")
+    BOT_OWNER = int(os.getenv("BOT_OWNER", 1534651884))
 #    OWNER_USERNAME = os.getenv("OWNER_USERNAME")
-    BACKUP_CHANNEL = os.getenv("BACKUP_CHANNEL", "real_MoviesAdda1")
+    BACKUP_CHANNEL = os.getenv("BACKUP_CHANNEL", "Primeflix_Cinema")
 #    GROUP_USERNAME = os.getenv("GROUP_USERNAME")
     START_MSG = os.getenv("START_MSG", '''Hᴇʏ Bᴜᴅᴅʏ! 😃
 
@@ -31,8 +31,8 @@ Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ Cʟɪᴄᴋ Oɴ Hᴇʟᴘ ✅''')
 ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴛʜᴇʀᴇ ʏᴏᴜʀ ʟɪɴᴋꜱ,
 ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏ ᴄʟɪᴄᴋ ᴏɴ ʜᴇʟᴘ ✅''')
     UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", None)
-    DATABASE_URL = os.getenv("DATABASE_URL", "")
-    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", ""))
+    DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://Primeflix5557:Primeflix5557@cluster0.d0tnqb6.mongodb.net/?retryWrites=true&w=majority")
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001875916506"))
     RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", 5))
     BROADCAST_AS_COPY = os.getenv("BROADCAST_AS_COPY", "True")
     UPDATES_CHANNEL_USERNAME = os.getenv("UPDATES_CHANNEL_USERNAME", "")
@@ -50,10 +50,10 @@ Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ Cʟɪᴄᴋ Oɴ Hᴇʟᴘ ✅''')
 🍓 Sᴛᴇᴘ 2 - ʙᴏᴛ ᴋᴏ ᴀᴘɴᴇ ɢʀᴏᴜᴘ ᴀᴜʀ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴋᴀ ᴀᴅᴍɪɴ ʙᴀɴᴀɴᴀ ʜᴏɢᴀ.
 
 🍓 Sᴛᴇᴘ 3 - ɢʀᴏᴜᴘ ᴍᴇ "/request" ᴛʏᴘᴇ ᴋᴀʀ ᴋᴇ ꜱᴇɴᴅ ᴋᴀʀɴᴀ ʜᴏɢᴀ!
-ꜰɪʀ ʙᴏᴛ ᴋᴇ ᴏᴡɴᴇʀ ᴀᴘᴋᴀ ʏᴇ ʀᴇQᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ᴋᴀʀ ʟᴇɴɢᴇ. @Help_Primeflix_Cinema
+ꜰɪʀ ʙᴏᴛ ᴋᴇ ᴏᴡɴᴇʀ ᴀᴘᴋᴀ ʏᴇ ʀᴇQᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ᴋᴀʀ ʟᴇɴɢᴇ. @yash_yadav99
 
 🍓 Sᴛᴇᴘ 4 - ɢʀᴏᴜᴘ ᴍᴇ "/addb - ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ɪᴅ" ᴛʏᴘᴇ ᴋᴀʀ ᴋᴇ ꜱᴇɴᴅ ᴋᴀʀɴᴀ ʜᴏɢᴀ.
-ꜰɪʀ ʙᴏᴛ ᴋᴇ ᴏᴡɴᴇʀ ᴀᴘᴋᴀ ʏᴇ ʀᴇQᴜᴇꜱᴛ ʙʜɪ ᴀᴄᴄᴇᴘᴛ ᴋᴀʀ ʟᴇɴɢᴇ @Help_Primeflix_Cinema
+ꜰɪʀ ʙᴏᴛ ᴋᴇ ᴏᴡɴᴇʀ ᴀᴘᴋᴀ ʏᴇ ʀᴇQᴜᴇꜱᴛ ʙʜɪ ᴀᴄᴄᴇᴘᴛ ᴋᴀʀ ʟᴇɴɢᴇ @yash_yadav99
 
 🍓 Sᴛᴇᴘ 5 - ᴀʙ ᴀᴘᴋᴏ ᴀᴘɴᴇ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ᴍᴇ ᴘᴏꜱᴛ ᴅᴀʟɴɪ ʜᴏɢɪ,
 ᴊɪꜱꜱᴇ ᴋɪ ᴀɢᴀʀ ɢʀᴏᴜᴘ ᴍᴇ ᴋᴏɪ ᴜꜱᴇʀ ꜱᴇᴀʀᴄʜ ᴋᴀʀᴇ ᴛᴏ ʏᴇ ʙᴏᴛ ᴜɴ ᴜꜱᴇʀ ᴋᴇ Qᴜᴀʀʏ ᴋᴏ ꜱᴀᴍᴀᴊʜ ᴋᴇ ᴀᴘᴋᴇ ᴄʜᴀɴɴᴇʟ ꜱᴇ ᴘᴏꜱᴛ ᴜᴛʜᴀ ᴋᴇ ᴜɴʜᴇ ᴅᴇ ᴘᴀʏᴇ.
@@ -61,4 +61,4 @@ Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ Cʟɪᴄᴋ Oɴ Hᴇʟᴘ ✅''')
 
 🍓 Nᴏᴛᴇ : Bᴏᴛ ᴀᴅᴍɪɴ ᴀᴘᴋᴇ ᴄʜᴀɴɴᴇʟ ᴍᴇ ᴊᴏɪɴ ʜᴏɴᴇ ᴄʜᴀʜɪʏᴇ,
 ᴀɢᴀʀ ʙᴏᴛ ᴀᴅᴍɪɴ ᴀᴘᴋᴀ ʀᴇQᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ɴʜɪ ᴋᴀʀ ʀᴀʜᴇ ʜᴀɪɴ ᴛᴏ ᴜɴʜᴇ ᴘᴇʀꜱᴏɴᴀʟ ᴍꜱɢ ᴋᴀʀᴇɴ.
-👉 @Help_Primeflix_Cinema''')
+👉 @yash_yadav99''')
